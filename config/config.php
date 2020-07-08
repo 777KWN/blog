f3fc1c6821eb80c515457ab105ac8632cfc19855
seu nome <kawanunes710@gmail.com>
2020-07-08 17:41:36 -0300

    segundo

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//SITE
+define('SITE_NAME','blog do kawan');
+//BANCO
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB','blog1');
+define('DRIVER','mysql');
+
+?>
